dfc932850e71f1af273513fc808ae6ffc74e706e
SydMontague <sydmontague@web.de>
2012-08-08 12:35:51 +0200

    update for 1.3

--- src/com/syd/expskills/ServerEntityListener.java
+++ src/com/syd/expskills/ServerEntityListener.java
@@ -45,12 +45,22 @@ public class ServerEntityListener implements Listener
             FileManager.savePF(p, pconfig);
             
             double level = p.getLevel() + p.getExp();
-            level = 1.75 * (level * level) + 4.5 * level;
+            int exp = 0;
+            while (level >= 1)
+            {
+                if (level - 16 > 0)
+                    exp += 17 + (level - 16) * 3;
+                else
+                    exp += 17;
+                
+                level--;
+            }
+            // level = 1.75 * (level * level) + 4.5 * level;
             
             // default usage of EXP Drop - add dynamic method with config usage
             Item item = p.getWorld().dropItem(p.getLocation(), new ItemStack(384, 1));
             item.setMetadata("expskills.exp", new FixedMetadataValue(plugin, xp));
-            item.setMetadata("expskills.level", new FixedMetadataValue(plugin, level));
+            item.setMetadata("expskills.level", new FixedMetadataValue(plugin, exp));
             
             event.setDroppedExp(0);
             death.setNewLevel(0);
@@ -67,24 +77,35 @@ public class ServerEntityListener implements Listener
             Item item = event.getItem();
             if (item.hasMetadata("expskills.exp"))
             {
-                
                 Player player = event.getPlayer();
                 
                 double level = player.getLevel() + player.getExp();
-                level = 1.75 * (level * level) + 4.5 * level;
-                level = level + item.getMetadata("expskills.level").get(0).asInt();
+                int exp = 0;
+                while (level >= 1)
+                {
+                    if (level - 16 > 0)
+                        exp += 17 + (level - 16) * 3;
+                    else
+                        exp += 17;
+                    
+                    level--;
+                }
                 
+                exp += item.getMetadata("expskills.level").get(0).asInt();
+                                
                 int i = 0;
-                double value;
                 do
                 {
                     i++;
-                    value = 1.75 * (i * i) + 4.5 * i;
+                    if (i - 16 > 0)
+                        exp = exp - 17 + (i - 16) * 3;
+                    else
+                        exp = exp - 17;
                 }
-                while (value < level);
-                
+                while (exp >= 17);
+                                
                 player.setTotalExperience(player.getTotalExperience() + item.getMetadata("expskills.exp").get(0).asInt());
-                player.setLevel(i-1);
+                player.setLevel(i);
                 event.getItem().remove();
                 event.setCancelled(true);
             }
